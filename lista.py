6538112b2f1aa42155f63f064e25efe013cf2d70
vitoriasantosgoncalves10466-web <vitoriasantosgoncalves10466@gmail.com>
2025-09-16 09:09:29 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,50 @@
+# lista = [1, 2, 3, 4]
+
+# lista.remove(2)
+# print(lista)
+
+# lista.pop(0)
+# print(lista)
+
+#Pesquisar e contar
+# lista = [1,2,3,2,2]
+# print(lista.index(3))
+# print(lista.count(2))
+
+# lista = [1, 2, 3, 4, 5]
+# print(len(lista))
+# print(max(lista))
+# print(min(lista))
+
+
+# #Atividade 01
+numeros = []
+numeros.append(10)
+numeros.append(20)
+numeros.append(30)
+print(numeros)
+numeros.insert(0, 15)
+print(numeros)
+
+#Atividade 02
+lista = [5, 10, 15, 20, 25]
+lista.remove(10)
+print(lista)
+lista.pop(0)
+print(lista)
+
+#Atividade 03
+valores = [8, 3, 10, 1, 7]
+print(valores)
+valores.sort()
+print(valores)
+valores.sort(reverse=True)
+print(valores)
+valores.reverse()
+print(valores)
+
+#Atividade 04
+notas = [7.5, 8.0, 6.5, 9.0, 5.5]
+print(len(notas))
+print(max(notas))
+print(min(notas))
---
+++ while_for.py
@@ -0,0 +1,34 @@
+# for numero in range(1, 16):
+#     print("Número for:", numero)
+
+# contador = 1
+# while contador <=15:
+#     print("Número while", contador)
+#     #contador = contador +1 #2
+#     contador += 1
+
+#Atividade 01
+for numero in range (1, 11):
+    print(numero)
+
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero += 1
+
+#Atividade 02
+for i in range (1, 6):
+    print(i)
+
+i = 1
+while i <=5:
+    print(i)
+    i += 1
+
+#Atividade 03
+for numero in range(0, 10, 2):
+    print(numero)
+
+while numero <=10:
+    print(numero)
+    numero +=2
